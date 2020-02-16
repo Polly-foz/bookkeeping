b730e0c150306a5bb82efdc4e44b4e214fcc5edd
polly-foz <polly.foz@gmail.com>
2020-02-16 14:50:59 +0800

    add 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>404 page</p>
+        <div>
+            <router-link to="/">返回首页</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFound"
+    };
+</script>
+
+<style scoped></style>
